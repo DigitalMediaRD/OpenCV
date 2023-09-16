a75f52dbd994acfe3af7eebcde70ed6474b42de6
QiFuChina <night7phoenix@gmail.com>
2023-09-16 21:41:33 +0800

    Update Splite&Merge

--- ImageProcessing/2.ImageProcessing/2.2ImageProcessing/2.2.5SeparatingRGB.py
+++ ImageProcessing/2.ImageProcessing/2.2ImageProcessing/2.2.5SeparatingRGB.py
@@ -1,6 +1,10 @@
 import cv2
+import numpy as np
+
 img=cv2.imread('res/img001.png',cv2.IMREAD_REDUCED_COLOR_2)
 cv2.imshow('OriginalImg',img)
+zeros=np.zeros(img.shape[:2], dtype = "uint8")
+print(zeros)
 def NumpySeparating():
     b=img[:,:,0]
     g=img[:,:,1]
@@ -15,9 +19,9 @@ def NumpySeparating():
 
 def CV2Splite():
     b,g,r=cv2.split(img)
-    cv2.imshow('B', b)
-    cv2.imshow('G', g)
-    cv2.imshow('R', r)
+    cv2.imshow('B', cv2.merge([b,zeros,zeros]))
+    cv2.imshow('G', cv2.merge([zeros,g,zeros]))
+    cv2.imshow('R', cv2.merge([zeros,zeros,r]))
     cv2.waitKey()
 
 
@@ -30,4 +34,4 @@ def Merge():
     cv2.waitKey()
 
 
-Merge()
+CV2Splite()
