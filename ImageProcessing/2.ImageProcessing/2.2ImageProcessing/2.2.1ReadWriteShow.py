839ef87e88ede10aa701cbad1cc55fa542818529
QiFuChina <110169005+fuqi0714@users.noreply.github.com>
2023-09-16 17:20:56 +0800

    Update ReadWriteShow

--- ImageProcessing/2.ImageProcessing/2.2ImageProcessing/2.2.1ReadWriteShow.py
+++ ImageProcessing/2.ImageProcessing/2.2ImageProcessing/2.2.1ReadWriteShow.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 def ImageRead():
     #img=cv2.imread('res/img001.png')
-    #img = cv2.imread('res/img001.png',cv2.IMREAD_UNCHANGED)
+    img = cv2.imread('res/img001.png',cv2.IMREAD_UNCHANGED)
     #img = cv2.imread('res/img001.png',cv2.IMREAD_GRAYSCALE)
     #img = cv2.imread('res/img001.png',cv2.IMREAD_COLOR)
     #img = cv2.imread('res/img001.png',cv2.IMREAD_ANYCOLOR)
-    img = cv2.imread('res/img001.png',cv2.IMREAD_REDUCED_COLOR_4)
-    print(img)
+    #img = cv2.imread('res/img001.png',cv2.IMREAD_REDUCED_COLOR_4)
+    print(img.shape)
     return img
 
 
@@ -25,4 +25,15 @@ def ImageShow():
     cv2.imshow('Show',ImageRead())
     cv2.waitKey(0)
 
-ImageShow()
+
+def ReadImgSaveAsOtherType():
+    ImageShow()
+    np.save("Img.npy",ImageRead())
+    ImageRead().tofile("Img.bin")
+    ReTransform=np.load("Img.npy")
+    cv2.imshow("TestNPY",ReTransform)
+    print(ReTransform.shape)
+    cv2.waitKey(0)
+
+
+ReadImgSaveAsOtherType()
